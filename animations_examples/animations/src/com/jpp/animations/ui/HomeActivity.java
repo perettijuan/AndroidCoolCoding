198434b476a5d56089209105622710e4fef56605
Juan Pablo Peretti <juan.p.peretti@intel.com>
2014-08-05 14:56:01 -0300

    New folder for better organization

---
+++ animations_examples/animations/src/com/jpp/animations/ui/HomeActivity.java
@@ -0,0 +1,160 @@
+package com.jpp.animations.ui;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.jpp.animations.AnimationGroups;
+import com.jpp.animations.Animations;
+import com.jpp.animations.R;
+import com.jpp.animations.ui.fragment.AnimationTypesFragment;
+import com.jpp.animations.ui.fragment.CustomAnimationsFragment;
+import com.jpp.animations.ui.fragment.ProgrammedSetAnimationFragment;
+import com.jpp.animations.ui.fragment.ProgrammedTranslateAnimationFragment;
+import com.jpp.animations.ui.fragment.SimpleScaleAnimationFragment;
+import com.jpp.animations.ui.fragment.SliderMenuFragment;
+
+public class HomeActivity extends ActionBarActivity implements ISldierMenuSelectionListener {
+
+
+    private static final String TITLE_KEY = "title_key";
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private SliderMenuFragment mSliderMenuFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        if (savedInstanceState != null) {
+            mTitle = savedInstanceState.getCharSequence(TITLE_KEY);
+            setTitle(mTitle);
+        } else {
+            mTitle = mDrawerTitle = getTitle();
+        }
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                // creates call to onPrepareOptionsMenu()
+                supportInvalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                // creates call to onPrepareOptionsMenu()
+                supportInvalidateOptionsMenu();
+
+                // hide keyword
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow((IBinder) getCurrentFocus().getWindowToken(), 0);
+            }
+        };
+
+        if (savedInstanceState == null) {
+            mSliderMenuFragment = SliderMenuFragment.newInstance(this);
+            getSupportFragmentManager().beginTransaction().add(R.id.left_drawer, mSliderMenuFragment).commit();
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        mSliderMenuFragment = (SliderMenuFragment) getSupportFragmentManager().findFragmentByTag(SliderMenuFragment.TAG);
+        if (mSliderMenuFragment != null) {
+            mSliderMenuFragment.changeListener(this);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putCharSequence(TITLE_KEY, mTitle);
+    }
+
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onAimationSelected(AnimationGroups group, Animations anim) {
+        Fragment fr = null;
+        switch (group) {
+        case SIMPLE_ANIMATIONS:
+            fr = SimpleScaleAnimationFragment.newInstance();
+            break;
+        case CUSTOM_ANIMATIONS:
+            fr = CustomAnimationsFragment.newInstance(anim);
+            break;
+        case ANIMATION_TYPES:
+            fr = AnimationTypesFragment.newInstance(anim);
+        case PROGRAMMED_ANIMATIONS:
+            switch (anim) {
+            case PROGRAMMED_TRANSLATE:
+                fr = ProgrammedTranslateAnimationFragment.newInstance(anim);
+                break;
+            case PROGRAMMED_ANIMATION_SET:
+                fr = ProgrammedSetAnimationFragment.newInstance(anim);
+            }
+            break;
+        default:
+            break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fr).commit();
+        mDrawerLayout.closeDrawers();
+
+    }
+
+}
---
+++ animations_examples/animations/src/com/jpp/animations/ui/fragment/SliderMenuFragment.java
@@ -0,0 +1,73 @@
+package com.jpp.animations.ui.fragment;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+
+import com.jpp.animations.AnimationGroups;
+import com.jpp.animations.Animations;
+import com.jpp.animations.R;
+import com.jpp.animations.ui.ISldierMenuSelectionListener;
+import com.jpp.animations.ui.adapter.SliderMenuAdapter;
+
+public class SliderMenuFragment extends Fragment {
+
+
+    public final static String TAG = SliderMenuFragment.class.getName();
+
+    private SliderMenuAdapter mAdapter;
+    private ExpandableListView lvDrawerMenu;
+    private ISldierMenuSelectionListener mListener;
+
+    public static SliderMenuFragment newInstance(ISldierMenuSelectionListener listener) {
+        SliderMenuFragment instance = new SliderMenuFragment();
+        instance.mListener = listener;
+        return instance;
+    }
+
+    public SliderMenuFragment() {
+
+    }
+
+    public void changeListener(ISldierMenuSelectionListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        List<AnimationGroups> headers = Arrays.asList(AnimationGroups.values());
+        HashMap<AnimationGroups, List<Animations>> items = new HashMap<AnimationGroups, List<Animations>>();
+        for (int i = 0; i < headers.size(); i++) {
+            AnimationGroups group = headers.get(i);
+            List<Animations> simpleAnimations = Arrays.asList(group.getAnimations());
+            items.put(group, simpleAnimations);
+        }
+
+        View fView = inflater.inflate(R.layout.slider_menu_fragment, container, false);
+        lvDrawerMenu = (ExpandableListView) fView.findViewById(R.id.lv_drawer_menu);
+        mAdapter = new SliderMenuAdapter(getActivity(), headers, items);
+        lvDrawerMenu.setAdapter(mAdapter);
+        lvDrawerMenu.setOnChildClickListener(new OnChildClickListener() {
+
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Animations child = (Animations) mAdapter.getChild(groupPosition, childPosition);
+                AnimationGroups group = (AnimationGroups) mAdapter.getGroup(groupPosition);
+                mListener.onAimationSelected(group, child);
+                return true;
+            }
+        });
+
+        return fView;
+    }
+}
