0542e81c134430563b2743456e225c4ed5be8e44
Juan Pablo Peretti <jpp@Juans-MacBook-Pro.local>
2018-11-22 11:19:31 -0300

    Add domain layer

--- ArchitectureComponents/app/src/main/java/com/jpp/architecturecomponents/domain/DomainItem.kt
+++ ArchitectureComponents/app/src/main/java/com/jpp/architecturecomponents/domain/DomainItem.kt
@@ -1,4 +1,6 @@
 package com.jpp.architecturecomponents.domain
 
-class DomainItem {
-}
+/**
+ * Our domain item: a very simple class that has only a name and an identifier.
+ */
+data class DomainItem(val name: String, val id: Int)
--- ArchitectureComponents/app/src/main/java/com/jpp/architecturecomponents/domain/GetItemsUseCase.kt
+++ ArchitectureComponents/app/src/main/java/com/jpp/architecturecomponents/domain/GetItemsUseCase.kt
@@ -1,2 +1,20 @@
 package com.jpp.architecturecomponents.domain
 
+/**
+ * This is the unique use case that the application has: simulates that retrieves a series of
+ * DomainItems from an external resource (an API).
+ * The work this use case executes is time consuming, with the main goal of simulating a server
+ * access for it.
+ *
+ * This class knows nothing about threading
+ */
+class GetItemsUseCase {
+    operator fun invoke(bottom: Int, top: Int): List<DomainItem> {
+        Thread.sleep(2000)
+        val listOfItems = mutableListOf<DomainItem>()
+        for (i in bottom..top) {
+            listOfItems.add(DomainItem("name", i))
+        }
+        return listOfItems
+    }
+}
